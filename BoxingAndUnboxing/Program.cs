82012a8f52dc111ce5f55f6aa1b7d8b046b2ac2f
Vahram Papazyan <vahram.developer@gmail.com>
2021-06-10 17:33:24 +0400

    Added another parto of code from BoxingAndUnboxing chapter

--- BoxingAndUnboxing/Program.cs
+++ BoxingAndUnboxing/Program.cs
@@ -12,6 +12,41 @@ namespace BoxingAndUnboxing
             return $"Point: x={x} y={y}";
         }
     }
+
+    internal struct Point_1 : IComparable
+    {
+        private readonly Int32 m_x, m_y;
+        // Constructor to easily initialize the fields
+        public Point_1(Int32 x, Int32 y)
+        {
+            m_x = x;
+            m_y = y;
+        }
+        // Override ToString method inherited from System.ValueType
+        public override String ToString()
+        {
+            // Return the point as a string. Note: calling ToString prevents boxing
+            return String.Format("({0}, {1})", m_x.ToString(), m_y.ToString());
+        }
+        // Implementation of type-safe CompareTo method
+        public Int32 CompareTo(Point_1 other)
+        {
+            // Use the Pythagorean Theorem to calculate
+            // which point is farther from the origin (0, 0)
+            return Math.Sign(Math.Sqrt(m_x * m_x + m_y * m_y)
+            - Math.Sqrt(other.m_x * other.m_x + other.m_y * other.m_y));
+        }
+        // Implementation of IComparable's CompareTo method
+        public Int32 CompareTo(Object o)
+        {
+            if (GetType() != o.GetType())
+            {
+                throw new ArgumentException("o is not a Point");
+            }
+            // Call type-safe CompareTo method
+            return CompareTo((Point)o);
+        }
+    }
     public sealed class Program
     {
         public static void Main()
@@ -43,6 +78,35 @@ namespace BoxingAndUnboxing
             Int32 x1 = 5;
             Object o1 = x; // Box x; o refers to the boxed object
             Int16 y1 = (Int16)(Int32)o; // Unbox to the correct type and cast
+            #endregion
+
+            #region Point_1 Main
+            // Create two Point_1 instances on the stack.
+            Point_1 p1 = new Point_1(10, 10);
+            Point_1 p2 = new Point_1(20, 20);
+            // p1 does NOT get boxed to call ToString (a virtual method).
+            Console.WriteLine(p1.ToString());// "(10, 10)"
+                                             // p DOES get boxed to call GetType (a non-virtual method).
+            Console.WriteLine(p1.GetType());// "Point_1"
+            // p1 does NOT get boxed to call CompareTo.
+            // p2 does NOT get boxed because CompareTo(Point) is called.
+            Console.WriteLine(p1.CompareTo(p2));// "-1"
+                                    // p1 DOES get boxed, and the reference is placed in c.
+            IComparable c = p1;
+            Console.WriteLine(c.GetType());// "Point"
+                                           // p1 does NOT get boxed to call CompareTo.
+                                           // Since CompareTo is not being passed a Point variable,
+                                           // CompareTo(Object) is called which requires a reference to
+                                           // a boxed Point.
+                                           // c does NOT get boxed because it already refers to a boxed Point.
+            Console.WriteLine(p1.CompareTo(c));// "0"
+                                               // c does NOT get boxed because it already refers to a boxed Point.
+                                               // p2 does get boxed because CompareTo(Object) is called.
+            Console.WriteLine(c.CompareTo(p2));// "-1"
+                                               // c is unboxed, and fields are copied into p2.
+            p2 = (Point_1)c;
+            // Proves that the fields got copied into p2.
+            Console.WriteLine(p2.ToString());// "(10, 10)"
             #endregion
         }
     }
