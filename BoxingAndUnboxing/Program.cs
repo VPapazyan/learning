4f1fdb5fa14066e18e88434f66f1ca8d7c228250
Vahram Papazyan <vahram.developer@gmail.com>
2021-06-10 13:48:06 +0400

    Added part of code form Boxing and Unboxing chapter

---
+++ BoxingAndUnboxing/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace BoxingAndUnboxing
+{
+    struct Point
+    {
+        public Int32 x, y;
+
+        public override string ToString()
+        {
+            return $"Point: x={x} y={y}";
+        }
+    }
+    public sealed class Program
+    {
+        public static void Main()
+        {
+            #region Boxing a struct
+            ArrayList a = new ArrayList();
+
+            Point p; // Allocate a Point (not in the heap).
+            for (Int32 i = 0; i < 10; i++)
+            {
+                p.x = p.y = i; // Initialize the members in the value type.
+                a.Add(p); // Box the value type and add the
+                          // reference to the Arraylist.
+            }
+
+            foreach (var item in a)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.Read();
+            #endregion
+
+            #region Boxing-unboxing rules
+            Int32 x = 5;
+            Object o = x; // Box x; o refers to the boxed object
+            Int16 y = (Int16)o; // Throws an InvalidCastException
+
+            Int32 x1 = 5;
+            Object o1 = x; // Box x; o refers to the boxed object
+            Int16 y1 = (Int16)(Int32)o; // Unbox to the correct type and cast
+            #endregion
+        }
+    }
+}
---
+++ NullableValueTypes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NullableValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region begining
+            Int32? n = null;
+            Object o = n; // o is null
+            Console.WriteLine("o is null={0}", o == null); // "True"
+            n = 5;
+            o = n; // o refers to a boxed Int32
+            Console.WriteLine("o's type={0}", o.GetType()); // "System.Int32"
+            #endregion
+
+            #region Unboxing nullable value types
+            // Create a boxed Int32
+            Object ob = 5;
+            // Unbox it into a Nullable<Int32> and into an Int32
+            Int32? a = (Int32?)ob; // a = 5
+            Int32 b = (Int32)ob; // b = 5
+                                // Create a reference initialized to null
+            o = null;
+            // "Unbox" it into a Nullable<Int32> and into an Int32
+            a = (Int32?)ob; // a = null
+            b = (Int32)ob; // NullReferenceException
+            #endregion
+        }
+    }
+}
