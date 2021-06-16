bd034b40be225443b558aec30a1090a4d4fe256f
Vahram Papazyan <vahram.developer@gmail.com>
2021-06-16 12:04:50 +0400

    Added solution of Excel task

---
+++ ReadExcelData/Program.cs
@@ -0,0 +1,46 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ReadExcelData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            List<Sales> excelData = new List<Sales>();
+            string file = @"..\..\..\Resources\SalesData.xlsx";
+
+            if (File.Exists(file))
+            {
+                using (var package = new ExcelPackage(new FileInfo(file)))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+
+                    for (int i = worksheet.Dimension.Start.Row + 1; i <= worksheet.Dimension.End.Row; i++)
+                    {
+                        if (worksheet.Cells[i, 1].Value != null)
+                        {
+                            excelData.Add(new Sales(
+                                worksheet.Cells[i, 1].Value.ToString(),
+                                worksheet.Cells[i, 2].Value.ToString(),
+                                (int)(double)worksheet.Cells[i, 3].Value,
+                                (double)worksheet.Cells[i, 4].Value
+                                ));
+                        }
+                    }
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException("There is no such file in the given directory");
+            }
+
+            Console.WriteLine($"You have {excelData.Count} sales. Total Sales is {excelData.Sum(s => s.UnitCost)}\n.");
+            excelData.Where(s => s.Unit >= 10).OrderBy(s => s.UnitCost).OrderBy(s => s.Rep).ToList().ForEach(s => Console.WriteLine(s));
+        }
+    }
+}
---
+++ ReadExcelData/Sales.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadExcelData
+{
+    class Sales
+    {
+        public string Rep { get; }
+        public string Item { get; }
+        public int Unit { get; }
+        public double UnitCost { get; }
+
+        public Sales(string rep, string item, int unit, double unitCost)
+        {
+            Rep = rep;
+            Item = item;
+
+            if (unit < 0)
+                throw new ArgumentException("Unit count can't be negative number");
+            else
+                Unit = unit;
+
+            if (unitCost < 0)
+                throw new ArgumentException("Unit cost count can't be negative number");
+            else
+                UnitCost = unitCost;
+        }
+
+        public override string ToString()
+        {
+            return $"{Rep,8} {Item,8} {Unit,5} {UnitCost,5}";
+        }
+    }
+}
